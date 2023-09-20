2cc2448d2e8571bc7b33bb5691cdb4d89840f6d6
60jong <rudwhd515@gmail.com>
2023-09-20 15:14:32 +0900

    setup : migrate to spring-boot 3.x / Java-17

---
+++ src/test/java/life/offonoff/ab/AbApplicationTests.java
@@ -0,0 +1,13 @@
+package life.offonoff.ab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
