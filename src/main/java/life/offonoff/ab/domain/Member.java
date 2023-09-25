8523775439f08925fb80f7a0a0b524057e773a57
60jong <rudwhd515@gmail.com>
2023-09-26 01:28:57 +0900

    feat : Member 엔티티 생성 + 생성-수정 시각 자동 기록용 config 추가

---
+++ src/main/java/life/offonoff/ab/domain/Member.java
@@ -0,0 +1,34 @@
+package life.offonoff.ab.domain;
+
+import jakarta.persistence.*;
+import life.offonoff.ab.domain.topic.Topic;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "member")
+    private List<Topic> publishedTopics = new ArrayList<>();
+
+    //== Constructor ==//
+    public Member(String name) {
+        this.name = name;
+    }
+
+    //== 연관관계 매핑 ==//
+    public void publishTopic(Topic topic) {
+        this.publishedTopics.add(topic);
+    }
+}
