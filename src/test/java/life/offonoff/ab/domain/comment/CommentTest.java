5c1263248bb7999af0c6e6c5144ffc96e9ee370a
60jong <rudwhd515@gmail.com>
2023-09-26 21:51:46 +0900

    test : Comment 연관관계 테스트 케이스 생성

---
+++ src/test/java/life/offonoff/ab/domain/comment/CommentTest.java
@@ -0,0 +1,32 @@
+package life.offonoff.ab.domain.comment;
+
+import life.offonoff.ab.domain.TestEntityUtil;
+import life.offonoff.ab.domain.member.Member;
+import life.offonoff.ab.domain.topic.Topic;
+import life.offonoff.ab.domain.topic.TopicSide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+    @Test
+    @DisplayName("댓글 작성 시 토픽의 댓글 수 증가")
+    void comment_count_increase() {
+        // given
+        int seq = 0;
+        Member member = TestEntityUtil.createMember(seq);
+        Topic topic = TestEntityUtil.createTextTopic(seq, TopicSide.TOPIC_A);
+
+        // when
+        Comment comment = TestEntityUtil.createComment(seq);
+        comment.associate(member, topic);
+
+        // then
+        assertAll(
+                () -> assertThat(topic.getCommentCount()).isEqualTo(1),
+                () -> assertThat(member.getComments()).contains(comment)
+        );
+    }
+}
