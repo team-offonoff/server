a0121e4974ee8f6fd603257cf9783d6fdb4e1400
60jong <rudwhd515@gmail.com>
2023-10-01 22:02:24 +0900

    feat : paging된 Topic (not hidden by a member) 조회하는 기능 구현

---
+++ src/main/java/life/offonoff/ab/repository/topic/TopicRepository.java
@@ -0,0 +1,27 @@
+package life.offonoff.ab.repository.topic;
+
+import life.offonoff.ab.domain.topic.Topic;
+import life.offonoff.ab.domain.topic.TopicStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    /**
+     * Paging, 모든 Topic조회 + Not Hidden by a Member
+     */
+    @Query("select t from Topic t " +
+            "join fetch t.category c " +
+            "where t.status = :status " +
+            "and not exists(select 1 from HiddenTopic ht " +
+                            "where ht.member.id = :memberId " +
+                            " and ht.topic.id = t.id)")
+    Slice<Topic> findAllNotHidden(
+            @Param("status") TopicStatus status,
+            @Param("memberId") Long memberId,
+            Pageable pageable
+    );
+}
