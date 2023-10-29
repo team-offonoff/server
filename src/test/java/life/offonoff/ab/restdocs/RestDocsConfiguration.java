56b02b08436fb5b61612cbecd72c3434f1936f06
melonturtle <hyeonjungbb@gmail.com>
2023-10-30 02:49:11 +0900

    docs: rest docs에 제목과 설명 일부 추가, 불필요한 헤더 제거

--- src/test/java/life/offonoff/ab/restdocs/RestDocsConfiguration.java
+++ src/test/java/life/offonoff/ab/restdocs/RestDocsConfiguration.java
@@ -4,7 +4,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 
 @TestConfiguration
 public class RestDocsConfiguration {
@@ -12,8 +13,13 @@ public class RestDocsConfiguration {
     public RestDocumentationResultHandler write() {
         return MockMvcRestDocumentation.document(
                 "{class-name}/{method-name}", // identifier
-                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
-                Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+                preprocessRequest(
+                        modifyHeaders()
+                                .remove("X-CSRF-TOKEN"), // 테스트에서 post 요청시 사용되는 CSRF 토큰 지우기
+                        modifyUris()
+                                .host("offonoff-ab-env.eba-aax22s52.ap-northeast-2.elasticbeanstalk.com"),
+                        prettyPrint()),
+                preprocessResponse(prettyPrint())
         );
     }
 }
