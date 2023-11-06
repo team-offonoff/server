a23585a754bfdde931c611ff3f7c8871317010e3
60jong <rudwhd515@gmail.com>
2023-11-06 23:51:46 +0900

    feat : kakao 인증 토큰 post 요청 기능 구현

---
+++ src/main/java/life/offonoff/ab/application/service/authenticate/oauth/OAuthRequestConst.java
@@ -0,0 +1,6 @@
+package life.offonoff.ab.application.service.authenticate.oauth;
+
+public class OAuthRequestConst {
+
+    public final static String POST_KAKAO_TOKEN_URL="https://kauth.kakao.com/oauth/token";
+}
---
+++ src/main/java/life/offonoff/ab/application/service/authenticate/oauth/OAuthRestTemplate.java
@@ -0,0 +1,57 @@
+package life.offonoff.ab.application.service.authenticate.oauth;
+
+import life.offonoff.ab.application.service.authenticate.oauth.profile.KakaoProfile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import static life.offonoff.ab.util.JwtParser.*;
+
+@RequiredArgsConstructor
+public class OAuthRestTemplate {
+
+    private final RestTemplate restTemplate;
+
+    public KakaoProfile getKakaoProfile(String authorizeCode, String redirectUri) {
+        KakaoTokenResponse response = restTemplate.postForEntity(
+                                                        OAuthRequestConst.POST_KAKAO_TOKEN_URL,
+                                                        createRequest(authorizeCode, redirectUri),
+                                                        KakaoTokenResponse.class)
+                                                  .getBody();
+        String payload = getPayload(response.getIdToken());
+        return (KakaoProfile) extractOAuthProfile(payload);
+    }
+
+    private HttpEntity<MultiValueMap<String, Object>> createRequest(String authorizeCode, String redirectUri) {
+        // Set Headers
+        HttpHeaders headers = getHeaders();
+        // Set Parameters
+        MultiValueMap<String, Object> params = getMultiValueMap(authorizeCode, redirectUri);
+
+        // Set HttpEntity
+        return new HttpEntity<>(params, headers);
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        return headers;
+    }
+
+    private MultiValueMap<String, Object> getMultiValueMap(String authorizeCode, String redirectUri) {
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "authorization_code");
+        map.add("client_id", "93647a5f60aa0f73d89af49f7ae3acbc");
+        map.add("code", authorizeCode);
+        map.add("redirect_uri", redirectUri);
+        return map;
+    }
+
+    private String getPayload(String idToken) {
+        return idToken.split("\\.")[1];
+    }
+}
