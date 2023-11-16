73dada7aad96ba2cfb177b47c7e1493737deb471
60jong <rudwhd515@gmail.com>
2023-11-16 21:36:24 +0900

    fix : @RequestBody 용 DTO에 기본 생성자 추가

--- src/main/java/life/offonoff/ab/application/service/request/auth/SignInRequest.java
+++ src/main/java/life/offonoff/ab/application/service/request/auth/SignInRequest.java
@@ -3,8 +3,10 @@ package life.offonoff.ab.application.service.request.auth;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
 
--- src/main/java/life/offonoff/ab/application/service/request/auth/SignUpRequest.java
+++ src/main/java/life/offonoff/ab/application/service/request/auth/SignUpRequest.java
@@ -3,17 +3,19 @@ package life.offonoff.ab.application.service.request.auth;
 import life.offonoff.ab.domain.member.Provider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequest {
 
     private String email;
     private String password;
-    private Provider provider;
+    private Provider provider = Provider.NONE;
 
     public void setEncodedPassword(String encoded) {
-        this.password = password;
+        this.password = encoded;
     }
 }
 
