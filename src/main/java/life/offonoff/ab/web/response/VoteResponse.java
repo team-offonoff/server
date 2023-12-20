b73985a5316eae9db4eb65e1b1f0f3628f65d817
60jong <rudwhd515@gmail.com>
2023-12-20 13:24:33 +0900

    rename : 최신 댓글 리턴... latestComment로 네이밍 변경

--- src/main/java/life/offonoff/ab/web/response/VoteResponse.java
+++ src/main/java/life/offonoff/ab/web/response/VoteResponse.java
@@ -1,6 +1,6 @@
 package life.offonoff.ab.web.response;
 
-public record VoteResponse(CommentResponse comment) {
+public record VoteResponse(CommentResponse latestComment) {
     public static VoteResponse from(CommentResponse comment) {
         return new VoteResponse(comment);
     }
