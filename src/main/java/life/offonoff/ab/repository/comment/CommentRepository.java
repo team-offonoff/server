4659e929389a89a6aa478023aad703897a70ef0a
melonturtle <hyeonjungbb@gmail.com>
2024-01-15 14:47:34 +0900

    fix: 댓글 삭제 시 멤버와 토픽이 모두 삭제되지 않게 한다

--- src/main/java/life/offonoff/ab/repository/comment/CommentRepository.java
+++ src/main/java/life/offonoff/ab/repository/comment/CommentRepository.java
@@ -2,6 +2,8 @@ package life.offonoff.ab.repository.comment;
 
 import life.offonoff.ab.domain.comment.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,5 +13,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
 
     int countAllByWriterIdAndTopicId(Long writerId, Long topicId);
 
+    @Modifying
+    @Query("delete from Comment c where c.writer.id = :writerId and c.topic.id = :topicId")
     int deleteAllByWriterIdAndTopicId(Long writerId, Long topicId);
 }
