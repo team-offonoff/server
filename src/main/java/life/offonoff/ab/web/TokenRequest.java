80c50109effaaffe2b6e07c775f7242adeb72111
60jong <rudwhd515@gmail.com>
2024-01-26 18:50:26 +0900

    test : 인증 토큰 재발급 테스트 시나리오 작성

---
+++ src/main/java/life/offonoff/ab/web/TokenRequest.java
@@ -0,0 +1,15 @@
+package life.offonoff.ab.web;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequest {
+
+    @JsonProperty(value = "refresh_token")
+    private String refreshToken;
+}
