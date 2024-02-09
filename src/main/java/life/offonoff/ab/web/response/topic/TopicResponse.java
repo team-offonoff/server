b8ad149ac2380fc17812a17114138dcd66a7d89d
60jong <rudwhd515@gmail.com>
2024-02-10 01:42:55 +0900

    refactor : TopicResponse 중복되는 생성자 호출문을 통일한다.

--- src/main/java/life/offonoff/ab/web/response/topic/TopicResponse.java
+++ src/main/java/life/offonoff/ab/web/response/topic/TopicResponse.java
@@ -7,64 +7,60 @@ import life.offonoff.ab.domain.topic.choice.ChoiceOption;
 import life.offonoff.ab.web.response.MemberResponse;
 import life.offonoff.ab.web.response.KeywordResponse;
 import life.offonoff.ab.web.response.topic.choice.ChoiceResponse;
+import life.offonoff.ab.web.response.topic.choice.ChoiceResponsesFactory;
 import life.offonoff.ab.web.response.topic.content.TopicContentResponseFactory;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 import static life.offonoff.ab.web.response.topic.content.TopicContentResponseFactory.*;
 
+@Getter
 @Builder
-public record TopicResponse(
-        Long topicId,
-        TopicSide topicSide,
-        String topicTitle,
-        Long deadline,
-        int voteCount,
-        int commentCount,
-        TopicContentResponse topicContent,
-        KeywordResponse keyword,
-        List<ChoiceResponse> choices,
-        MemberResponse author,
-        Long createdAt,
-        ChoiceOption selectedOption
-) {
+@AllArgsConstructor
+public class TopicResponse {
+    private Long topicId;
+    private TopicSide topicSide;
+    private String topicTitle;
+    private Long deadline;
+    private int voteCount;
+    private int commentCount;
+    private TopicContentResponse topicContent;
+    private KeywordResponse keyword;
+    private List<? extends ChoiceResponse> choices;
+    private MemberResponse author;
+    private Long createdAt;
+    private ChoiceOption selectedOption;
+
+    public TopicResponse(Topic topic, Member retriever) {
+        this.topicId = topic.getId();
+        this.topicSide = topic.getSide();
+        this.topicTitle = topic.getTitle();
+        this.deadline = topic.getDeadlineSecond();
+        this.voteCount = topic.getVoteCount();
+        this.topicContent = TopicContentResponseFactory.create(topic.getContent());
+        this.keyword = KeywordResponse.from(topic.getKeyword());
+        this.choices = ChoiceResponsesFactory.create(topic);
+        this.author = MemberResponse.from(topic.getAuthor());
+        this.createdAt = topic.getCreatedSecond();
+        this.selectedOption = getSelectedOption(topic, retriever);
+    }
+
+    private ChoiceOption getSelectedOption(Topic topic, Member retriever) {
+        if (retriever == null) {
+            return null;
+        }
+        return retriever.getVotedOptionOfTopic(topic);
+    }
+
     public static TopicResponse from(Topic topic) {
-        return new TopicResponse(
-                topic.getId(),
-                topic.getSide(),
-                topic.getTitle(),
-                topic.getDeadlineSecond(),
-                topic.getVoteCount(),
-                topic.getCommentCount(),
-                TopicContentResponseFactory.create(topic.getContent()),
-                KeywordResponse.from(topic.getKeyword()),
-                topic.getChoices().stream().map(ChoiceResponse::from).toList(),
-                MemberResponse.from(topic.getAuthor()),
-                topic.getCreatedSecond(),
-                null // 선택 option이 없을 경우 null 처리
-        );
+        return new TopicResponse(topic, null);
     }
 
     public static TopicResponse from(Topic topic, Member retriever) {
-        ChoiceOption retrieversVotedOption = retriever.getVotedOptionOfTopic(topic);// TODO : topics.size * votes.size 시간 복잡도 개선
-        return TopicResponse.from(topic, retrieversVotedOption);
-    }
-
-    public static TopicResponse from(Topic topic, ChoiceOption retrieversVotedOption) {
-        return new TopicResponse(
-                topic.getId(),
-                topic.getSide(),
-                topic.getTitle(),
-                topic.getDeadlineSecond(),
-                topic.getVoteCount(),
-                topic.getCommentCount(),
-                TopicContentResponseFactory.create(topic.getContent()),
-                KeywordResponse.from(topic.getKeyword()),
-                topic.getChoices().stream().map(ChoiceResponse::from).toList(),
-                MemberResponse.from(topic.getAuthor()),
-                topic.getCreatedSecond(),
-                retrieversVotedOption
-        );
+        // TODO : topics.size * votes.size 시간 복잡도 개선
+        return new TopicResponse(topic, retriever);
     }
 }
