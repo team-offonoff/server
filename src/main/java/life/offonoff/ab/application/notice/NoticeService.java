e179c60463203d09d6bb073df26f68929cc199f1
60jong <rudwhd515@gmail.com>
2024-02-14 19:32:19 +0900

    feat : NotificationType 생성한다.

--- src/main/java/life/offonoff/ab/application/notice/NoticeService.java
+++
@@ -1,8 +0,0 @@
-package life.offonoff.ab.application.notice;
-
-import life.offonoff.ab.domain.vote.VoteResult;
-
-public interface NoticeService {
-
-    void noticeVotingResult(VoteResult result);
-}
--- src/main/java/life/offonoff/ab/domain/notice/NotificationType.java
+++ src/main/java/life/offonoff/ab/domain/notice/NotificationType.java
@@ -1,5 +1,9 @@
 package life.offonoff.ab.domain.notice;
 
 public class NotificationType {
-    public static final String VOTING_RESULT_NOTIFICATION = "VOTING_RESULT";
+    public static final String DEFAULT = "DEFAULT";
+    public static final String VOTE_RESULT_NOTIFICATION = "VOTE_RESULT";
+    public static final String LIKE_IN_COMMENT_NOTIFICATION = "LIKE_IN_COMMENT";
+    public static final String COMMENT_ON_TOPIC_NOTIFICATION = "COMMENT_ON_TOPIC";
+    public static final String VOTE_COUNT_ON_TOPIC_NOTIFICATION = "VOTE_COUNT_ON_TOPIC";
 }
