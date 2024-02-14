aedc169de74009cab8309aceeea67db98393b530
60jong <rudwhd515@gmail.com>
2024-02-14 19:36:12 +0900

    feat : NotificationType에 따른 알림 클래스 생성한다.

---
+++ src/main/java/life/offonoff/ab/domain/notice/CommentOnTopicNotification.java
@@ -0,0 +1,20 @@
+package life.offonoff.ab.domain.notice;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import life.offonoff.ab.domain.member.Member;
+import life.offonoff.ab.domain.vote.VoteResult;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static life.offonoff.ab.domain.notice.NotificationType.COMMENT_ON_TOPIC_NOTIFICATION;
+// TODO:수정대상
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@DiscriminatorValue(COMMENT_ON_TOPIC_NOTIFICATION)
+public class CommentOnTopicNotification extends Notification {
+}
